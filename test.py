4978ad534b92506a334797cbe21a8fe33bc41605
Mariam Hasanat <201011@ppu.edu.ps>
2024-02-01 19:01:06 +0200

    prepare 1, delete the habal 😁

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = 'static'
-ALLOWED_EXTENSIONS = {"mp4"}
-
-app = Flask(__name__)
-app.config['static'] = UPLOAD_FOLDER
